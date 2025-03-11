8f455f60d8893b38b2acbd0969302fbb4a665ffb
Hernan <hernan24744@gmail.com>
2025-03-11 14:20:26 -0300

    uso de middlewares y rutas

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// middlewares nativos
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Routes
+app.use("/", require("./routes/users.route.js"));
+app.use("/", require("./routes/movies.route.js"));
+
 app.listen(PORT, () => {
   `Srvidor corriento en el puerto ${PORT}`;
 });
